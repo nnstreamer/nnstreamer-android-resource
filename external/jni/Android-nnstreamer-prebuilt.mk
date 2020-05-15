9e672b2286ada82437121480517cc17c252b4538
Jaeyun <jy1210.jung@samsung.com>
2020-05-15 18:42:28 +0900

    [Android] prebuilt libs for subplugins
    define prebuilt libraries for each sub-plugins.
    
    Signed-off-by: Jaeyun <jy1210.jung@samsung.com>

--- external/jni/Android-nnstreamer-prebuilt.mk
+++ external/jni/Android-nnstreamer-prebuilt.mk
@@ -11,107 +11,101 @@ NNSTREAMER_DIR := $(LOCAL_PATH)/nnstreamer
 NNSTREAMER_INCLUDES := $(NNSTREAMER_DIR)/include
 NNSTREAMER_LIB_PATH := $(NNSTREAMER_DIR)/lib/$(TARGET_ARCH_ABI)
 
+ENABLE_TF_LITE := false
 ENABLE_SNAP := false
 ENABLE_NNFW := false
+ENABLE_SNPE := false
 
 #------------------------------------------------------
-# nnstreamer native
+# define required libraries for nnstreamer
+#------------------------------------------------------
+NNSTREAMER_LIBS := nnstreamer nnstreamer-native gst-android cpp-shared
+
+#------------------------------------------------------
+# nnstreamer
 #------------------------------------------------------
 include $(CLEAR_VARS)
+LOCAL_MODULE := nnstreamer
+LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libnnstreamer.so
+include $(PREBUILT_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := nnstreamer-native
-
 LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libnnstreamer-native.so
-
 include $(PREBUILT_SHARED_LIBRARY)
 
 #------------------------------------------------------
 # gstreamer android
 #------------------------------------------------------
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := gst-android
-
 LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libgstreamer_android.so
-
 include $(PREBUILT_SHARED_LIBRARY)
 
 #------------------------------------------------------
 # c++ shared
 #------------------------------------------------------
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := cpp-shared
-
 LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libc++_shared.so
-
 include $(PREBUILT_SHARED_LIBRARY)
 
 #------------------------------------------------------
-# snap sdk (arm64-v8a only)
+# tensorflow-lite
 #------------------------------------------------------
-ifeq ($(ENABLE_SNAP), true)
+ifeq ($(ENABLE_TF_LITE),true)
 include $(CLEAR_VARS)
-
-LOCAL_MODULE := snap-sdk
-
-LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libsnap_vndk.so
-
+LOCAL_MODULE := tensorflow-lite-subplugin
+LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libtensorflow-lite-subplugin.so
 include $(PREBUILT_SHARED_LIBRARY)
+
+NNSTREAMER_LIBS += tensorflow-lite-subplugin
 endif
 
 #------------------------------------------------------
-# NNFW prebuilt libraries (arm64-v8a only)
+# SNAP (arm64-v8a only)
 #------------------------------------------------------
-ifeq ($(ENABLE_NNFW), true)
-NNFW_PREBUILT_LIBS :=
+ifeq ($(ENABLE_SNAP),true)
+SNAP_LIB_PATH := $(NNSTREAMER_LIB_PATH)
+include $(LOCAL_PATH)/Android-snap-prebuilt.mk
 
 include $(CLEAR_VARS)
-NNFW_PREBUILT_LIBS += nnfw-libbackend_cpu
-LOCAL_MODULE := nnfw-libbackend_cpu
-LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libbackend_cpu.so
+LOCAL_MODULE := snap-subplugin
+LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libsnap-subplugin.so
 include $(PREBUILT_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-NNFW_PREBUILT_LIBS += nnfw-libcircle_loader
-LOCAL_MODULE := nnfw-libcircle_loader
-LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libcircle_loader.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-NNFW_PREBUILT_LIBS += nnfw-libneuralnetworks
-LOCAL_MODULE := nnfw-libneuralnetworks
-LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libneuralnetworks.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-NNFW_PREBUILT_LIBS += nnfw-libneurun_core
-LOCAL_MODULE := nnfw-libneurun_core
-LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libneurun_core.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-NNFW_PREBUILT_LIBS += nnfw-libnnfw-dev
-LOCAL_MODULE := nnfw-libnnfw-dev
-LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libnnfw-dev.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-NNFW_PREBUILT_LIBS += nnfw-libtflite_loader
-LOCAL_MODULE := nnfw-libtflite_loader
-LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libtflite_loader.so
-include $(PREBUILT_SHARED_LIBRARY)
+NNSTREAMER_LIBS += $(SNAP_PREBUILT_LIBS) snap-subplugin
 endif
 
 #------------------------------------------------------
-# define required libraries for nnstreamer
+# NNFW (arm64-v8a only)
 #------------------------------------------------------
-NNSTREAMER_LIBS := nnstreamer-native gst-android cpp-shared
+ifeq ($(ENABLE_NNFW),true)
+NNFW_LIB_PATH := $(NNSTREAMER_LIB_PATH)
+include $(LOCAL_PATH)/Android-nnfw-prebuilt.mk
 
-ifeq ($(ENABLE_SNAP), true)
-NNSTREAMER_LIBS += snap-sdk
+include $(CLEAR_VARS)
+LOCAL_MODULE := nnfw-subplugin
+LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libnnfw-subplugin.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+NNSTREAMER_LIBS += $(NNFW_PREBUILT_LIBS) nnfw-subplugin
 endif
 
-ifeq ($(ENABLE_NNFW), true)
-NNSTREAMER_LIBS += $(NNFW_PREBUILT_LIBS)
+#------------------------------------------------------
+# SNPE
+#------------------------------------------------------
+ifeq ($(ENABLE_SNPE),true)
+SNPE_LIB_PATH := $(NNSTREAMER_LIB_PATH)
+include $(LOCAL_PATH)/Android-snpe-prebuilt.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := snpe-subplugin
+LOCAL_SRC_FILES := $(NNSTREAMER_LIB_PATH)/libsnpe-subplugin.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+NNSTREAMER_LIBS += $(SNPE_PREBUILT_LIBS) snpe-subplugin
 endif
+
+# Remove any duplicates.
+NNSTREAMER_LIBS := $(sort $(NNSTREAMER_LIBS))
